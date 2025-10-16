f82b6ab38e5f65674a22371910a0b99b0c20eef3
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-10-16 19:25:28 -0300

    feat: estrutura inicial das rotas de aluno

--- Codigo/Back/sistema-de-moeda/src/main/java/com/app/sistema_de_moeda/controllers/AlunoController.java
+++ Codigo/Back/sistema-de-moeda/src/main/java/com/app/sistema_de_moeda/controllers/AlunoController.java
@@ -1,10 +1,41 @@
 package com.app.sistema_de_moeda.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.app.sistema_de_moeda.services.AlunoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/alunos")
+@RequiredArgsConstructor
 public class AlunoController {
+    private final AlunoService alunoService;
+
+    @GetMapping
+    public ResponseEntity<List<AlunoDto>> buscarAlunos() {
+        return ResponseEntity.ok(alunoService.buscarAlunos());
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> criarAluno(@RequestBody AlunoDto alunoDto) {
+        alunoService.criarAluno(alunoDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AlunoDto> buscarAluno(@PathVariable Long id) {
+        return ResponseEntity.ok(alunoService.buscarAluno(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletarAluno(@PathVariable Long id) {
+        return ResponseEntity.ok(alunoService.deletarAluno(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AlunoDto> editarAluno(@PathVariable Long id, @RequestBody AlunoDto alunoDto) {
+        alunoService.editarAluno(id, alunoDto);
+    }
 
 }
