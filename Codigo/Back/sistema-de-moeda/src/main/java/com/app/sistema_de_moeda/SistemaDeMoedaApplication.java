fad1e5533a032caa8480b86c482dd3ed9c70b27b
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-10-16 19:02:52 -0300

    feat: template inicial do spring boot

---
+++ Codigo/Back/sistema-de-moeda/src/main/java/com/app/sistema_de_moeda/SistemaDeMoedaApplication.java
@@ -0,0 +1,13 @@
+package com.app.sistema_de_moeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeMoedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeMoedaApplication.class, args);
+	}
+
+}
---
+++ Codigo/Back/sistema-de-moeda/src/test/java/com/app/sistema_de_moeda/SistemaDeMoedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sistema_de_moeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeMoedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
