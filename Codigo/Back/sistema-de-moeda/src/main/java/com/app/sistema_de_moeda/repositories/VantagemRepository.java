189f97dbef65a7eca1c2de3cdd37659a21b5df03
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-11-27 17:43:49 -0300

    feat: repositorio de vantagem

---
+++ Codigo/Back/sistema-de-moeda/src/main/java/com/app/sistema_de_moeda/repositories/VantagemRepository.java
@@ -0,0 +1,7 @@
+package com.app.sistema_de_moeda.repositories;
+
+import com.app.sistema_de_moeda.models.Vantagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VantagemRepository extends JpaRepository<Vantagem, Long> {
+}
